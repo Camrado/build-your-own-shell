8cf10a04801d10d02c3140fcfb3cc798043c6fec
Camrado <blackandwhite.b17w@gmail.com>
2026-07-05 15:24:24 +0400

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -3,5 +3,8 @@ class Program
     static void Main()
     {
         Console.Write("$ ");
+        
+        var input = Console.ReadLine();
+        Console.WriteLine($"{input}: command not found");
     }
 }
