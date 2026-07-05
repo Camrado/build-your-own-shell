4a4fd196c3fcfedde7c07106215dc4e5fb56d8ea
Camrado <blackandwhite.b17w@gmail.com>
2026-07-05 15:33:21 +0400

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -3,5 +3,10 @@ while (true)
     Console.Write("$ ");
 
     var input = Console.ReadLine();
+    if (input == "exit")
+    {
+        break;
+    }
+    
     Console.WriteLine($"{input}: command not found");
 }
