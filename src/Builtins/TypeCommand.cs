c6325d9910451142bb4d86d6056b02a5cd3091c8
Camrado <blackandwhite.b17w@gmail.com>
2026-07-05 16:22:24 +0400

    codecrafters submit [skip ci]

--- src/Builtins/TypeCommand.cs
+++ src/Builtins/TypeCommand.cs
@@ -40,4 +40,33 @@ public static class TypeCommand
 
         return null;
     }
+
+    private static bool IsExecutable(string filePath)
+    {
+        if (!File.Exists(filePath))
+            return false;
+
+        return OperatingSystem.IsWindows()
+            ? HasExecutableExtensionWindows(filePath)
+            : HasUnixExecutePermission(filePath);
+    }
+
+    private static bool HasExecutableExtensionWindows(string filePath)
+    {
+        var pathExt = Environment.GetEnvironmentVariable("PATHEXT");
+        var extensions = pathExt?.Split(";") ?? [".COM", ".EXE", ".BAT", ".CMD"];
+        
+        var fileExtension = Path.GetExtension(filePath);
+        
+        return extensions.Any(ext => string.Equals(ext, fileExtension, StringComparison.OrdinalIgnoreCase));
+    }
+
+    private static bool HasUnixExecutePermission(string filePath)
+    {
+        var mode = File.GetUnixFileMode(filePath);
+        
+        return mode.HasFlag(UnixFileMode.UserExecute) ||
+               mode.HasFlag(UnixFileMode.GroupExecute) ||
+               mode.HasFlag(UnixFileMode.OtherExecute);
+    }
 }
